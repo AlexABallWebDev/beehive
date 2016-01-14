087f2ec28dcb2f482c78e21c84c4453523615422
AlexABallWebDev <aball6@mail.greenriver.edu>
2016-01-14 11:08:23 -0800

    moved secure data and changes paths to access db connection

--- index.php
+++ index.php
@@ -64,7 +64,7 @@ if (isset($_POST['submit']))
 	if (empty($errorArray))
 	{
 		//get database connection called $beeDBConnection
-		require('/home/aball/secure-includes/bee-db-connection.php');
+		require('/home/alexb/secure-includes/bee-db-connection.php');
 		
 		/*
 		//escape data to help prevent sql injection
